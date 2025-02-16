3071a36e4204fbfa09b9cdcc855cbb7242b2076b
HalahanNikolai <133275902+HalahanNikolai@users.noreply.github.com>
2025-02-16 17:45:27 +0200

    Problem  with modal (routerDom)

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
 import './index.css'
 // import App from './App.jsx'
-import App from './components/App/App';
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+
   // <StrictMode>
   //   <App />
   // </StrictMode>,
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
